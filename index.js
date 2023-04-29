7ed86bd5a8a747c634cb4e0285df8afc161f597d
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 10:40:39 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'vuslat';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'maybe vuslat'
+    return leastFavoriteCustomer;
+}
+
